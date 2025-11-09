ff9dfab3abbbf54e584794f589454867e96a0d49
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-09 09:48:07 +0530

    mutable and immutable objects in datatypes

---
+++ 01_DataTypes/chapter_1.py
@@ -0,0 +1,10 @@
+# Immutable Data Types Example
+sugar_amount = 2
+print(f"Initial sugar: {sugar_amount}")
+
+
+sugar_amount = 12
+print(f"second Initial sugar: {sugar_amount}")
+
+print(f"ID of 2: {id(2)}")
+print(f"ID of 12: {id(12)}")
---
+++ 01_DataTypes/chapter_2.py
@@ -0,0 +1,8 @@
+spice_mix = set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+print(f"Initial spice mix id: {spice_mix}")
+spice_mix.add("cinnamon")
+spice_mix.add("cardamon")
+spice_mix.add("clove")
+print(f"After spice mix id: {spice_mix}")
+print(f"After spice mix id: {id(spice_mix)}")
