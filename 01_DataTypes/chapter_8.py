150513eccaaabebd2a170a2d88c5658f4457019c
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-10 12:28:28 +0530

    Lists in Python

---
+++ 01_DataTypes/chapter_8.py
@@ -0,0 +1,38 @@
+# Lists (From this here they can be changed this are Mutables)
+
+
+#Adding the element to list
+ingredients = ["water", "milk", "black tea"]
+ingredients.append("sugar")
+print(f"Ingredients are: {ingredients}")
+
+#Removing th element from list
+ingredients.remove("water")
+print(f"Ingredients are: {ingredients}")
+
+#Extend the list
+car_brands = ["BMW", "Mercedes"]
+car_specifications = ["super charged", "petrol"]
+car_specifications.extend(car_brands)
+print(f"Cars: {car_specifications}")
+
+#Inserting 
+car_specifications.insert(2,"drifting") #indexing 
+print(f"After adding: {car_specifications}") 
+last_added = car_specifications.pop()
+print(f"{last_added}")
+print(f"After adding: {car_specifications}") 
+
+#reversing list
+car_specifications.reverse()
+print(f"After revresing: {car_specifications}") 
+
+#sorting list
+car_specifications.sort()
+print(f"After Sorting: {car_specifications}") 
+
+
+# levels - Max, Min
+sugar_levels = [1,2,3,4,5]
+print(f"Maximum sugar level: {max(sugar_levels)}")
+print(f"Minimum sugar level: {min(sugar_levels)}")
