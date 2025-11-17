daee9980811ce0851e60931c2a7666a83a09f8b1
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-17 13:09:18 +0530

    Dictionarys in python

--- 01_DataTypes/python_10.py
+++ 01_DataTypes/python_10.py
@@ -14,3 +14,21 @@ print(f"Recipe: {chai_recipi}")
 
 del chai_recipi["liquid"]
 print(f"Recipe: {chai_recipi}")
+
+print(f"Is sugar in the order? : {'sugar' in chai_order}")
+
+chai_order = {"type": "ginger chai", "size":"Medium", "sugar":1}
+# print(f"Order details (keys): {chai_order.keys()}")
+# print(f"Order details (values): {chai_order.values()}")
+# print(f"Order details (items): {chai_order.items()}")
+
+last_item = chai_order.popitem()
+print(f"Removed last item: {last_item}")
+
+extra_spices = {"cardamon": "crushed", "ginger": "sliced"}
+chai_recipi.update(extra_spices)
+print(f"Updated Extra spices : {chai_recipi}")
+
+
+customer_note = chai_order.get("size", "NO Note")
+print(f"chai Size is :{customer_note}")
