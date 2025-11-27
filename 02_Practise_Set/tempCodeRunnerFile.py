2784de2919e2b9e4e37373ae1d5ecce36c542071
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-27 18:37:16 +0530

    match case solved problems

--- 02_Practise_Set/tempCodeRunnerFile.py
+++ 02_Practise_Set/tempCodeRunnerFile.py
@@ -1,6 +1,9 @@
-number = int(input("Enter a number: "))
-
-# if number % 2 == 0:
-#   print(f"{number} is Even Number")
-# else :
-#   print(f"{number} is Odd Number")
+units = int(input("Enter the number of electricity units consumed:"))
+# match units:
+#   case units if units <= 100:
+#     bill = units * 5
+#     print("Electricity bill is: ₹", bill)
+#   case units if 101 <= units <= 300:
+#     print("Electricity bill is: ₹", units * 8)
+#   case units if units > 300:
+#     print("Electricity bill is: ₹", units * 10)
