a45133fe11d734b4202a013d84c166965cb5ce15
Rakshitih <rakshithkumab01718@gmail.com>
2025-12-01 22:41:08 +0530

    listing through for lop

---
+++ 03_Loops/Looping_through_list.py
@@ -0,0 +1,11 @@
+# You receive a list of names for chai orders.
+# The goal is to print out the order queue.
+
+# Task:
+# *Use a list of names.
+# *Print: "Order ready for[names]"
+
+list_names = ["RK", "Jayanth", "RJ", "Prajwal"]
+
+for list in list_names:
+  print(f"The Order ready for : {list}")
