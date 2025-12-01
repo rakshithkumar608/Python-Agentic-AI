964a27ef25b7763ee460ed3c81e7a347ef3f129c
Rakshitih <rakshithkumab01718@gmail.com>
2025-12-01 22:25:26 +0530

    token dispenser with for loop

---
+++ 03_Loops/tea_token_despenser.py
@@ -0,0 +1,10 @@
+# A tea Stall owner has a digital token display.
+# For every customer in line,a  token number is printed and chai is served.
+
+# Task:
+# * Use a for loop to generate token numbers from 1 to 10 using range().range
+# *Print: "Serving chai to Token #[number]"
+
+
+for token in range(1, 11):
+  print(f"Serving chai to Token #{token}")
