69a55c0c654b38a32ccfbe0660f24298279965a4
Rakshitih <rakshithkumab01718@gmail.com>
2025-12-02 22:19:36 +0530

    Walrus concepts

---
+++ 03_Loops/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+value = 13
+
+# if (remainder := value % 5):
+#   print(f"Not divisible, reminder is {remainder}")
+
+
+
+# available_sizes = ["small", "medium", "large"]
+
+# if (requested_size := input("Enter your chai cup size:")) in available_sizes:
+#   print(f"serving {requested_size}")
+# else:
+#   print(f"Size is unavailable - {requested_size}")
