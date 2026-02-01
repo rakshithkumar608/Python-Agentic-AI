8b908d9376726894a517a662d44107b4824e64d0
Rakshitih <rakshithkumab01718@gmail.com>
2026-02-01 22:37:00 +0530

    feat: Add `log_activity` decorator for logging function calls.

---
+++ 08_decorators/02_Login_decorator.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+
+def log_activity(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"🚀 Calling: {func.__name__}")
+        result = func(*args, **kwargs)
+        print(f"✅ Finished {func.__name__}")
+        return result
+    return wrapper
+
+
+@log_activity
+def brew_chai(type):
+    print(f"Brewing {type} chai")
+
+brew_chai("Masala")
