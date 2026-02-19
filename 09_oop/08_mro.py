cbf0ede0ebadd8c36afe69cf267395fe2d67958b
Rakshitih <rakshithkumab01718@gmail.com>
2026-02-19 06:36:12 +0530

    feat: Add example demonstrating Python's Method Resolution Order (MRO) with multiple inheritance.

---
+++ 09_oop/08_mro.py
@@ -0,0 +1,18 @@
+class A:
+    label = "A: Base class"
+
+
+class B(A):
+    label = "B: Masala blend"
+
+
+class C(A):
+    label = "C: Special blend"
+
+
+class D(B, C):
+    pass
+
+cup = D()
+print(cup.label)
+print(D.__mro__)
