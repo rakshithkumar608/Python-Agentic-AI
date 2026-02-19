bbf4bf334ef29d86862ad7dc1768a7ff43d305f3
Rakshitih <rakshithkumab01718@gmail.com>
2026-02-19 06:28:30 +0530

    feat: Add `Chai` base class and `GingerChai` subclass demonstrating inheritance and different parent constructor calls.

--- 09_oop/07_base_class.py
+++ 09_oop/07_base_class.py
@@ -7,7 +7,7 @@ class GingerChai(Chai):
     def __init__(self, type_, strength, spice_level):
         self.type = type_
         self.strength = strength
-        self.spice_level = spice_level
+        self.spice_level = spice_level 
 
 
 class GingerChai(Chai):
