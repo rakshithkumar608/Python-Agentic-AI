5ef6f6d20efa856b28983e085a981670b6f05a97
Rakshith Kumar <rakshithkumab01718@gmail.com>
2026-03-14 08:21:46 +0530

    Add initial implementation of text generation using transformers pipeline

---
+++ hf_basics/main.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="google/gemma-3-270m-it")
+
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe(messages)
